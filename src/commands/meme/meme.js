ea6301578db62fefd7f5336b183cee27c1b9c429
Furkan Ergüldürenler <furkanerguldurenler@hotmail.com>
2026-09-15 02:00:08 +0300

    refactor: Rename command from 'memes' back to 'meme' for consistency

--- src/commands/meme/meme.js
+++ src/commands/meme/meme.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require('discord.js');
 const { infoEmbed, errorEmbed } = require('../../utils/embeds');
 
 module.exports = {
-  data: new SlashCommandBuilder().setName('memes').setDescription('Get a random meme.'),
+  data: new SlashCommandBuilder().setName('meme').setDescription('Rastkele meme getirir.'),
   async execute(interaction) {
     await interaction.deferReply();
 
